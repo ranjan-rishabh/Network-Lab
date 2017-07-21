2824a99dd389e543f63c7072e06b03786ccb3d17
Rishabh Ranjan <rishabh.ranjan015@gmail.com>
2017-07-21 11:02:27 +0530

    Update Assignment1.java
    6)Print the classes of the ip address.

--- Assignment1.java
+++ Assignment1.java
@@ -16,6 +16,7 @@ public class Assignment1 {
             String s = in.next();
             InetAddress ip1 = InetAddress.getByName(s);
             System.out.println("3)IP Address Remote Host:"+ip1.getHostAddress());
+            String s1 = ip1.getHostAddress();
             System.out.println("Enter Remote IP Address:");
             s = in.next();
             ip1 = InetAddress.getByName(s);
@@ -28,6 +29,28 @@ public class Assignment1 {
 		sb.append(String.format("%02X%s", macAddress[i], (i < macAddress.length - 1) ? "-" : ""));
             }
             System.out.println("4)Local Mac Address:"+sb.toString());
+            sb.setLength(0);
+            for(int i=0;i<s1.length();i++)
+            {
+                if(s1.charAt(i)=='.')
+                    break;
+                else
+                    sb.append(s1.charAt(i));
+            }
+            String s2 = sb.toString();
+            int i = Integer.parseInt(s2);
+            if(i>=1&&i<=126)
+                System.out.println("A");
+            else if(i<192)
+                System.out.println("B");
+            else if(i<224)
+                System.out.println("C");
+            else if(i<240)
+                System.out.println("D");
+            else if(i<256)
+                System.out.println("E");
+            else
+                System.out.println("No Class Found");               
         }
         catch(Exception e)
         {
